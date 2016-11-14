2a247fbbac1e0fafd97c6982a99bcbc771f14a4c
Artem Klevakin <artemklv@yandex.ru>
2016-11-15 03:35:47 +0500

    emulate production server

--- commands/build.js
+++ commands/build.js
@@ -1,6 +1,7 @@
 let config = require('../webpack.config')
 let webpack =  require('webpack')
 let HtmlWebpackPlugin = require('html-webpack-plugin')
+var WebpackDevServer = require('webpack-dev-server')
 
 process.env.NODE_ENV = 'production'
 const GLOBALS = {
@@ -37,4 +38,20 @@ config.plugins = config.plugins.concat([
   })
 ])
 
-module.exports = config
+// add hot module replacement
+config.entry = ['webpack-dev-server/client?http://localhost:3000'].concat(config.entry)
+
+new WebpackDevServer(webpack(config), {
+  publicPath: config.output.publicPath,
+  hot: true,
+  inline: false,
+  historyApiFallback: true,
+  quiet: true
+}).listen(3000, 'localhost', function (error, stats) {
+  if (error) { // so a fatal error occurred. Stop here.
+    console.log(error)
+    return 1
+  }
+  console.log('production server run at http://localhost:3000')
+})
+
